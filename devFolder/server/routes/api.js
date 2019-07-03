a41c5e314b17a9b85444caf59c94104b75ffd79b
dragonz4827 <Patrick.Tim.Moore@gmail.com>
2019-07-03 09:58:05 -0500

    Added additional commenting and instructions

--- devFolder/server/routes/api.js
+++ devFolder/server/routes/api.js
@@ -26,11 +26,27 @@
             response.send(results);
         });
     }
+    /**
+     * Gets a list of constituent IDs
+     * @name getConstituentCodes
+     * @param {Object} request
+     * @param {Object} response
+     * @param {int} request.params.offset
+     * @returns {array[int]}
+     */
     function getConstituentCodes(request, response) {
         Sky.getConstituentCodes(request, request.params.offset, function (results) {
             response.send(results);
         });
     }
+    /**
+     * Gets a list of constituent relations by ID
+     * @name getConstituentRelationships
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentRelationships(request, response) {
         Sky.getConstituentRelationships(request, request.params.constituentId, function (results) {
             /*fs.appendFile(dateString, (JSON.stringify(results, null, '\t') + '\n'), (err) => {
@@ -40,6 +56,14 @@
             response.send(results);
         });
     }
+    /**
+     * Gets constituent first name by ID
+     * @name getConstituentFirstName
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentFirstName(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, (JSON.stringify(results, null, '\t') + '\n'), (err) => {
@@ -49,6 +73,14 @@
             response.send(results);
         });
     }
+    /**
+     * Gets constituent first name by ID and prints to file
+     * @name getConstituentNamePre
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentNamePre(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
           if(typeof results.first != "undefined"){
@@ -62,6 +94,14 @@
           response.send(results);
         });
     }
+    /**
+     * Gets constituent first name by ID and prints to file
+     * @name getConstituentNamePost
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentNamePost(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, ('{ "first": ' + JSON.stringify(results.first, null, '\t') + ', "middle": ' + JSON.stringify(results.middle, null, '\t') + ' }\n'), (err) => {
@@ -72,6 +112,14 @@
           }
         );
     }
+    /**
+     * Gets constituent first name by ID and prints to file
+     * @name patchConstituentFirstNamePre
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function patchConstituentFirstNamePre(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, (results.id.toString() + ':\n\t{ "first": ' + JSON.stringify(results.first, null, '\t') + ' }'), (err) => {
@@ -82,6 +130,14 @@
           }
         );
     }
+    /**
+     * Gets constituent first name by ID and prints to file
+     * @name patchConstituentFirstNamePost
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function patchConstituentFirstNamePost(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, ('{ "first": ' + JSON.stringify(results.first, null, '\t') + ' }\n'), (err) => {
@@ -92,6 +148,15 @@
           }
         );
     }
+    /**
+     * Patches constituent first name by ID and prints to file
+     * @name patchConstituentFirstName
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @param {string} request.params.firstName
+     * @returns {string}
+     */
     function patchConstituentFirstName(request, response) {
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "first": request.params.firstName }, function (results) {
           fs.appendFile(dateString, " => ", (err) => {
@@ -101,6 +166,16 @@
           response.send(results);
         });
     }
+    /**
+     * Patches constituent first and middle by ID and prints to file
+     * @name patchConstituentName
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @param {string} request.params.firstName
+     * @param {string} request.params.midInitial
+     * @returns {string}
+     */
     function patchConstituentName(request, response) {
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "first": request.params.firstName, "middle": request.params.midInitial }, function (results) {
           fs.appendFile(dateString, " => ", (err) => {
@@ -110,12 +185,28 @@
           response.send(results);
         });
     }
+    /**
+     * Gets constituent title by ID
+     * @name getConstituentTitlePreTest
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentTitlePreTest(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             response.send(results);
           }
         );
     }
+    /**
+     * Gets constituent title by ID and prints to file
+     * @name getConstituentTitlePostTest
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentTitlePostTest(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, (results.id.toString() + ':\n\t{ "first": ' + JSON.stringify(results.first, null, '\t') + ' } + { "title": ' + 'TBD' + ' }\n'), (err) => {
@@ -125,6 +216,13 @@
           }
         );
     }
+    /**
+     * Symbolizes a patched constituent title by ID and prints to file
+     * @name patchConstituentTitleTest
+     * @param {Object} request
+     * @param {Object} response
+     * @returns {string}
+     */
     function patchConstituentTitleTest(request, response) {
           fs.appendFile(dateString, " + ", (err) => {
             if (err) console.log(err);
@@ -132,6 +230,14 @@
           });
           response.send();
     }
+    /**
+     * Gets constituent title by ID and prints to file
+     * @name getConstituentTitlePreLive
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentTitlePreLive(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             if(typeof results.title === "undefined"){
@@ -143,6 +249,14 @@
           }
         );
     }
+    /**
+     * Gets constituent title by ID and prints to file
+     * @name getConstituentTitlePostLive
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getConstituentTitlePostLive(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, (' + { "title": ' + 'TBD' + ' }\n'), (err) => {
@@ -152,6 +266,13 @@
           }
         );
     }
+    /**
+     * Symbolically patches constituent title by ID and prints to file
+     * @name patchConstituentTitleLive
+     * @param {Object} request
+     * @param {Object} response
+     * @returns {string}
+     */
     function patchConstituentTitleLive(request, response) {
           fs.appendFile(dateString, ' + ', (err) => {
             if (err) console.log(err);
@@ -159,10 +280,26 @@
           });
           response.send();
     }
+    /**
+     * Uses the USA Census to assume gender
+     * @name guessGender
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.firstName
+     * @returns {JSON object}
+     */
     function guessGender(request, response) {
           console.log("Gender: assumed.");
           response.send(guess.guess(request.params.firstName));
     }
+    /**
+     * Translates the first collumn of a CSV file to an array
+     * @name getCSVid
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.fileName
+     * @returns {array[string]}
+     */
     function getCSVid(request, response) {
         console.log(request.params.fileName);
           fs.readFile(request.params.fileName + '.csv', (err, fd) => {
@@ -181,6 +318,14 @@
             response.send(arrayNew);
           });
     }
+    /**
+     * Gets constituent title/gender by ID and prints to file
+     * @name getTGPre
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getTGPre(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
           if(typeof results.first != "undefined" && (typeof results.title === "undefined" || (typeof results.gender === "undefined" || results.gender == 'Unknown'))){
@@ -192,6 +337,14 @@
           response.send(results);
         });
     }
+    /**
+     * Gets constituent title/gender by ID and prints to file
+     * @name getTGPost
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function getTGPost(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
             fs.appendFile(dateString, ('{ "first": ' + JSON.stringify(results.first, null, '\t') + ', "title": ' + JSON.stringify(results.title, null, '\t') + ', "gender": ' + JSON.stringify(results.gender, null, '\t') + ' }'), (err) => {
@@ -202,6 +355,15 @@
           }
         );
     }
+    /**
+     * Patches constituent title/gender by ID and prints to file
+     * @name patchTG
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @param {string} request.params.gender
+     * @returns {string}
+     */
     function patchTG(request, response) {
       if(request.params.gender == 'Male'){
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "gender": request.params.gender, "title": 'Mr.' }, function (results) {
@@ -221,6 +383,15 @@
         });
       }
     }
+    /**
+     * Patches constituent title by ID and prints to file
+     * @name patchTitle
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @param {string} request.params.gender
+     * @returns {string}
+     */
     function patchTitle(request, response) {
       if(request.params.gender == 'Male'){
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "title": 'Mr.' }, function (results) {
@@ -240,6 +411,15 @@
         });
       }
     }
+    /**
+     * Patches constituent gender by ID and prints to file
+     * @name patchGender
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @param {string} request.params.gender
+     * @returns {string}
+     */
     function patchGender(request, response) {
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "gender": request.params.gender }, function (results) {
           fs.appendFile(dateString, ' => ', (err) => {
@@ -249,6 +429,14 @@
           response.send(results);
         });
     }
+    /**
+     * Patches constituent title by ID and prints to file
+     * @name titleSwap
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function titleSwap(request, response) {
         Sky.patchConstituentFirstName(request, request.params.constituentId, { "title": 'Mrs.' }, function (results) {
           fs.appendFile(dateString, ' => ', (err) => {
@@ -258,6 +446,14 @@
           response.send(results);
         });
     }
+    /**
+     * Gets constituent title by ID and prints to file
+     * @name preTitleSwap
+     * @param {Object} request
+     * @param {Object} response
+     * @param {string} request.params.constituentId
+     * @returns {string}
+     */
     function preTitleSwap(request, response) {
         Sky.getConstituent(request, request.params.constituentId, function (results) {
           if(typeof results.title != "undefined" && results.title == 'Ms.'){
@@ -269,6 +465,7 @@
           response.send(results);
         });
     }
+    /* exports functions to global variables */
     module.exports = {
         getConstituent: getConstituent,
         getConstituentCodes: getConstituentCodes,
